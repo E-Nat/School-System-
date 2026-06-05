8e8eab90d9acd3a1d7153deb3d195d01dbf94ab7
Samboblue123 <sumsambo8899@gmail.com>
2026-06-05 23:29:47 +0700

    update v2

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
     <h1>hello world</h1>
     <h1>Welcome to my website</h1>
     <p>This is a simple HTML page.</p>
