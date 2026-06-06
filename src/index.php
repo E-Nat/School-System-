f1d81d536b1d084356dc02dd916078fd2cf66eaa
Samboblue123 <sumsambo8899@gmail.com>
2026-06-06 19:06:07 +0700

    Setup Login Page

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <h1>hello world</h1>
+    <h1>Welcome to my website</h1>
+    <p>This is a simple HTML page.</p>
+    <footer>
+        <p>&copy; 2023 My Website. All rights reserved.</p>
+    </footer>
+    
+</body>
+</html>
