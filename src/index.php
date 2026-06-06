f4406e59a6355f4e07e7b36a32f3095a31206a53
Samboblue123 <sumsambo8899@gmail.com>
2026-06-06 23:08:10 +0700

    Test 01

---
+++ src/index.php
@@ -0,0 +1,4 @@
+<?php
+<h1>Welcome to the School System</h1>
+<p>This is the main page of the School System. Please navigate to the appropriate section to manage students, teachers, classes, and more.</p>
+?>
