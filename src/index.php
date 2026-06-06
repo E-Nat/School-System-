145ade26eb1dc090e06cc8f9c89a09823604bb24
Ann-Boy <angaminggkh0@gmail.com>
2026-06-06 23:36:49 +0700

    hello world

--- src/index.php
+++ src/index.php
@@ -1,4 +1,1 @@
-<?php
-<h1>Welcome to the School System</h1>
-<p>This is the main page of the School System. Please navigate to the appropriate section to manage students, teachers, classes, and more.</p>
-?>
+<?php echo "Hello, World!"; ?>
